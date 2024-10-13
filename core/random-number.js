2c23d0d49c43e707d37046b1edf1f61b5fd93b38
annikiy.skryabin <annikiy.skryabin@simpleone.ru>
2024-10-13 16:27:22 +0300

    lesson-7-add-jsdoc

--- core/random-number.js
+++ core/random-number.js
@@ -1,5 +1,12 @@
 
 export default class RandomNumber {
+
+    /**
+     * Возвращает случайное число
+     *
+     * @static
+     * @returns {number}
+     */
     static get() {
         return Math.round(Math.random() * 1_000_000);
     }
